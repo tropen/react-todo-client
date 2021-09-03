e386519d676642c67364f067003318681c0fa904
tropen <tropen09@gmail.com>
2021-09-03 19:29:14 +0300

    break app to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
 import React from 'react';
-import { Button } from "@material-ui/core";
-import { useSelector, useDispatch } from "react-redux";
-import { FETCH_AUTH_KEY } from "./constants/ActionTypes";
+import { BrowserRouter as Router } from "react-router-dom";
+import Content from "./components/Content";
+import Header from "./components/Header";
 
-
-function App() {
-  const list = useSelector(state => state.authKey);
-  const dispatch = useDispatch();
-
+const App = () => {
   return (
-    <>
-      <Button
-        color="primary"
-        onClick={() => {
-          console.log('fetch AuthKey with saga -> add to state -> listener will save it to the localStorage');
-          return dispatch({ type: FETCH_AUTH_KEY });
-        }}>Sign in</Button>
-
-      <div>
-        <h1>{list}</h1>
-      </div>
-      {/*<header></header>*/}
-      {/*<Content></Content>*/}
-    </>
+    <Router>
+      <Header />
+      <Content />
+    </Router>
   );
-}
+};
 
 export default App;
