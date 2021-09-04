17e286ec8e05ae8e830a410388eba3dd1a3de5a6
tropen <tropen09@gmail.com>
2021-09-04 22:32:33 +0300

    add Toast reducer

---
+++ src/reducers/toast.jsx
@@ -0,0 +1,26 @@
+import { SET_TOAST } from "../constants/ActionTypes";
+
+const initialState = {
+  open: false,
+  message: null,
+  severity: 'error'
+};
+
+export default function toast(state = initialState, action) {
+  switch (action.type) {
+    case SET_TOAST: {
+      const { open, message, severity } = action;
+      return {
+        ...state,
+        open,
+        severity,
+        message
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+export const showMessage = (msg, severity ='error') => ({type: SET_TOAST, open: true, msg, severity});
+export const closeMessage = (msg, severity ='error') => ({type: SET_TOAST, open: false, msg, severity});
