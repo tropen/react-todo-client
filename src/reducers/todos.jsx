a3114360a283bb3c4ee6c099c742992e482f7b8c
tropen <tropen09@gmail.com>
2021-09-05 00:53:18 +0300

    fix error with array-to-object

--- src/reducers/todos.jsx
+++ src/reducers/todos.jsx
@@ -1,13 +1,24 @@
-import { SAVE_TODOS_TO_STORAGE, REMOVE_TODO_FROM_STORAGE, ADD_TODO_TO_STORAGE } from '../constants/ActionTypes';
+import {
+  SAVE_TODOS_TO_STORAGE,
+  REMOVE_TODO_FROM_STORAGE,
+  ADD_TODO_TO_STORAGE,
+  TOGGLE_TODO_IN_STORAGE, SIGN_OUT
+} from '../constants/ActionTypes';
+import * as types from "../constants/ActionTypes";
 
 export default function todos(state = [], action) {
   switch (action.type) {
     case SAVE_TODOS_TO_STORAGE:
       return action.todos;
     case REMOVE_TODO_FROM_STORAGE:
-      return state.filter(todo => todo.id!==action.id);
+      return state.filter(todo => todo.id !== action.id);
     case ADD_TODO_TO_STORAGE:
-      return state.filter(todo => todo.id!==action.id);
+      return state.filter(todo => todo.id !== action.id);
+    case TOGGLE_TODO_IN_STORAGE:
+      state.find(todo => todo.id === action.id)['done'] = action.done;
+      return [...state,];
+    case SIGN_OUT:
+      return [];
     default:
       return state;
   }
@@ -16,3 +27,5 @@ export default function todos(state = [], action) {
 export const saveTodosToStorage = todos => ({ type: SAVE_TODOS_TO_STORAGE, todos });
 export const removeTodoFromStorage = id => ({ type: REMOVE_TODO_FROM_STORAGE, id });
 export const addTodoFromStorage = todo => ({ type: ADD_TODO_TO_STORAGE, todo });
+export const toggleTodo = (id, done) => ({ type: types.TOGGLE_TODO, id, done });
+export const toggleTodoInStorage = (id, done) => ({ type: types.TOGGLE_TODO_IN_STORAGE, id, done });
