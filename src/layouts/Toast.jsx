3037d456d0b30a65acdb5faf873883ec8ce3e50b
tropen <tropen09@gmail.com>
2021-09-04 22:51:22 +0300

    add my toast (snackbar)

--- src/layouts/Toast.jsx
+++ src/layouts/Toast.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Snackbar from "@material-ui/core/Snackbar";
 import Alert from "@material-ui/lab/Alert";
 import { useDispatch, useSelector } from "react-redux";
-import { closeMessage } from "../reducers/snackbar";
+import { closeMessage } from "../reducers/toast";
 import { Slide } from "@material-ui/core";
 
 export const Toast = () => {
   const dispatch = useDispatch();
-  const open = useSelector(state => state.snackbar.open);
-  const message = useSelector(state => state.snackbar.message);
-  const severity = useSelector(state => state.snackbar.severity);
+  const open = useSelector(state => state.toast.open);
+  const message = useSelector(state => state.toast.message);
+  const severity = useSelector(state => state.toast.severity);
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import authKey from './authKey';
 import todos from "./todos";
 import users from "./users";
+import toast from "./toast";
 
 export default combineReducers({
   authKey,
   todos,
   users,
+  toast,
 });
 
 //todo add  FETCH_FAILED and test
