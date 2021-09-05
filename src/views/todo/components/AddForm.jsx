d44dc3277fbaf96fcba4ee80b01aec43005faaaf
tropen <tropen09@gmail.com>
2021-09-06 02:48:03 +0300

    update add form

--- src/views/todo/components/AddForm.jsx
+++ src/views/todo/components/AddForm.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import { Field, Form } from 'react-final-form';
-import { Box, Button, Grid, TextField, Typography, } from "@material-ui/core";
-//, MenuItem, Select
+import { Form } from 'react-final-form';
+import { Box, Button, Grid, MenuItem, Typography } from "@material-ui/core";
+import { Checkboxes, DateTimePicker, Select, TextField } from 'mui-rff';
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
+import DateFnsUtils from '@date-io/date-fns';
+import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../../../actions";
 
-const onSubmit = async (values) => {
-  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-  await sleep(300);
+const validate = (values) => {
+  const errors = {};
+  const checks = ['title', 'user_id', 'task', 'limit'];
+  checks.forEach((field) => {
+    if (!values[field]) {
+      errors[field] = 'Required';
+    }
+  });
 
-  console.log("submit", values);
-  window.alert(JSON.stringify(values, 0, 2));
+  return errors;
 };
-
-// const validate = (values) => {
-//   const errors = {};
-//   console.log('validate', values);
-//   if (!values.title) {
-//     errors.title = 'Required';
-//   }
-//
-//   return errors;
-// };
 
 const useStyles = makeStyles((theme) => ({
   box: {
@@ -31,8 +29,40 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const AddForm = () => {
+const AddForm = ({ users }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const onSubmit = async (values) => {
+    if (values.done === undefined) {
+      values.done = false;
+    }
+    dispatch(addTodo(values));
+  };
+
+  const userMenuItems = users.map((user) =>
+    <MenuItem key={user.id} value={user.id}>
+      {user.username}
+    </MenuItem>
+  );
+  const select =
+    <Grid item xs={6}>
+      <Select
+        name="user_id"
+        label="User"
+        formControlProps={{ margin: 'none' }}
+        MenuProps={{
+          anchorOrigin: {
+            vertical: "bottom",
+            horizontal: "left"
+          },
+          getContentAnchorEl: null
+        }}
+      >
+        {userMenuItems}
+      </Select>
+    </Grid>;
+
   return (
     <Box p={2} m={2} border={2} borderRadius={8} className={classes.box} variant="outlined">
       <Typography variant="h4" align="center" component="h1" gutterBottom>
@@ -40,39 +70,39 @@ const AddForm = () => {
       </Typography>
       <Form
         onSubmit={onSubmit}
-        // validate={validate}
+        validate={validate}
         initialValues={{}}
-        render={({ handleSubmit, values, submitting }) => (
+        render={({ handleSubmit, submitting }) => (
           <form onSubmit={handleSubmit} noValidate>
-            {/*user
-              title
-              task
-              limit
-              done
-              */}
             <Grid container alignItems="flex-start" spacing={2}>
               <Grid item xs={6}>
-                <Field name="title" id="title"
-                       component={TextField}
-                       label="Title" required
-                       type="text"
+                <TextField name="title"
+                           label="Title"
+                           type="text"
+                />
+              </Grid>
+
+              {select}
+
+              <Grid item xs={6}>
+                <DateTimePicker
+                  name="limit"
+                  label="Date and time limit"
+                  dateFunsUtils={DateFnsUtils}
                 />
               </Grid>
               <Grid item xs={6}>
-                <Field name="task"
-                       color="secondary"
-                       component={TextField}
-                       label="Task"
-                       required
-                       type="text"
+                <Checkboxes name="done"
+                            formControlProps={{ margin: 'none' }}
+                            data={{ label: 'Done', value: true }}
                 />
               </Grid>
-              <Grid item style={{ marginTop: 16 }}>
-                <Button color="primary"
-                        variant="contained"
-                        type="submit"
-                        disabled={submitting}
-                >Submit</Button>
+              <Grid item xs={12}>
+                <TextField name="task"
+                           label="Task"
+                           type="text"
+                           fullWidth
+                />
               </Grid>
               <Grid item style={{ marginTop: 16 }}>
                 <Button color="secondary"
@@ -81,6 +111,13 @@ const AddForm = () => {
                         disabled={submitting}
                 >Cancel</Button>
               </Grid>
+              <Grid item style={{ marginTop: 16 }}>
+                <Button color="primary"
+                        variant="contained"
+                        type="submit"
+                        disabled={submitting}
+                >Submit</Button>
+              </Grid>
             </Grid>
           </form>
         )}
@@ -88,5 +125,9 @@ const AddForm = () => {
     </Box>
 
   );
+};
+
+AddForm.propTypes = {
+  users: PropTypes.array
 };
 export default AddForm;
