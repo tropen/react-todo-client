c8c8c6eeed3644a2b79c29d8aa06fbafe4f131ba
tropen <tropen09@gmail.com>
2021-09-06 20:14:24 +0300

    update msg

--- src/sagas/auth.jsx
+++ src/sagas/auth.jsx
@@ -1,13 +1,16 @@
 import { call, put } from "redux-saga/effects";
 import { apiCall } from "../util/axiosApi";
 import { saveAuthKeyToStorage } from "../reducers/authKey";
-import { apiCallFailed } from "./index";
+
+import { showMessage } from "../reducers/toast";
+import { SUCCESS } from "../constants/messageTypes";
 
 export function* fetchAuthKey() {
   try {
     const data = yield call(apiCall, { method: 'POST', url: '/signin' });
     yield put(saveAuthKeyToStorage(data.authKey));
+    yield put(showMessage("Welcome!", SUCCESS));
   } catch (error) {
-    yield put(apiCallFailed(error));
+    yield put(showMessage(error));
   }
 }
